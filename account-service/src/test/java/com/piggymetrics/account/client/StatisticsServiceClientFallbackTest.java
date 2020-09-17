1b70cbdeafb4e77d8f76340cbf33c89a21867389
shipeng <pensh@microsoft.com>
2020-09-17 14:07:23 +0800

    fix test class compile error

--- account-service/src/test/java/com/piggymetrics/account/client/StatisticsServiceClientFallbackTest.java
+++ account-service/src/test/java/com/piggymetrics/account/client/StatisticsServiceClientFallbackTest.java
@@ -7,9 +7,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.hamcrest.Matchers.containsString;
 
 /**
@@ -24,11 +23,10 @@ public class StatisticsServiceClientFallbackTest {
     private StatisticsServiceClient statisticsServiceClient;
 
     @Rule
-    public final OutputCapture outputCapture = new OutputCapture();
+    public final OutputCaptureRule outputCapture = new OutputCaptureRule();
 
     @Before
     public void setup() {
-        outputCapture.reset();
     }
 
     @Test
